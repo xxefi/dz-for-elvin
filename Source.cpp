c792b2c293b9b204ae091a349b6d23c2b0e3b89a
xxefi <134388584+xxefi@users.noreply.github.com>
2023-06-01 17:36:07 +0400

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Circle {
+private:
+	double radius;
+public:
+    Circle(double r) : radius(r) {}
+
+    double getRadius() const {
+        return radius;
+    }
+
+    bool operator==(const Circle& other) const {
+        return radius == other.radius;
+    }
+
+    bool operator>(const Circle& other) const {
+        return radius > other.radius;
+    }
+    Circle& operator+=(double value) {
+        radius += value;
+        return *this;
+    }
+
+    Circle& operator-=(double value) {
+        radius -= value;
+        return *this;
+    }
+};
+
+
+
+
+int main() {
+	setlocale(LC_ALL, "");
+    Circle circle1(5.0);
+    Circle circle2(3.0);
+    if (circle1 == circle2) {
+        cout << "Радиусы окружностей равны";
+    }
+    else {
+        cout << "Радиусы окружностей не равны!";
+    }
+}
